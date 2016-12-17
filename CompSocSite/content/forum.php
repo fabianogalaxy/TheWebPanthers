94d7ef2fc153abffd2680d6e034e60806a333b0f
Fabio <fabianogalaxy@gmail.com>
2016-12-17 14:21:06 +0000

    New member welcome page

---
+++ CompSocSite/content/forum.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                Blog: Here is where the blog page will be placed   
+                    
+                                      
+            </div>
+            <hr>
+        </div>
+     </div>
+    </body>
+</html>
---
+++ CompSocSite/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('func.php');
+require_once('header.php');
+
+$carrega_pagina = carrega_pagina();
+require_once('footer.php');
