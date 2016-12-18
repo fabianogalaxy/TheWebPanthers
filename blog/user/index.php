6ce1cbf4a2da69e2a83775e80f150d7395611c82
x16114442 <x16114442@student.ncirl.ie>
2016-12-18 01:27:56 +0000

    Add files via upload

---
+++ blog/user/index.php
@@ -0,0 +1,54 @@
+<?php
+//include config
+require_once('../includes/config.php');
+
+//if not logged in redirect to login page
+if(!$user->is_logged_in()){ header('Location: login.php'); }
+
+//show message from add / edit page
+if(isset($_GET['delpost'])){ 
+
+	$stmt = $db->prepare('DELETE FROM blog_posts WHERE postID = :postID') ;
+	$stmt->execute(array(':postID' => $_GET['delpost']));
+
+	header('Location: index.php?action=deleted');
+	exit;
+} 
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>User Home</title>
+  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="../style/main.css">
+  <script language="JavaScript" type="text/javascript">
+  function delpost(id, title)
+  {
+	  if (confirm("Are you sure you want to delete '" + title + "'"))
+	  {
+	  	window.location.href = 'index.php?delpost=' + id;
+	  }
+  }
+  </script>
+</head>
+<body>
+
+	<div id="wrapper">
+
+	<?php include('menu.php');?>
+
+	<?php 
+	//show message from add / edit page
+	if(isset($_GET['action'])){ 
+		echo '<h3>Post '.$_GET['action'].'.</h3>'; 
+	} 
+	?>
+
+	
+
+</div>
+
+</body>
+</html>
