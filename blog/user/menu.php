3265bfa5c230dbd0c9b3247f8a9932439681f078
x16114442 <x16114442@student.ncirl.ie>
2016-12-18 18:34:53 -0500

    delete

--- blog/user/menu.php
+++
@@ -1,8 +0,0 @@
-<h1>Blog</h1>
-<ul id='adminmenu'>
-	
-	<li><a href="../" target="_blank">View All Blogs</a></li>
-	<li><a href='logout.php'>Logout</a></li>
-</ul>
-<div class='clear'></div>
-<hr />
